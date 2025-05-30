07c14349cc915d06b1ed155a4cc62e797bbafca6
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-31 00:26:40 +0600

    Setting up monorepo using NX

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,28 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
 
-const host = process.env.HOST ?? 'localhost';
+const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello API" });
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+const myPort = process.env.PORT || 6001;
+const server = app.listen(myPort, () => {
+  console.log(`Listening on port: ${myPort}`);
+});
+
+server.on("error", (err) => {
+  console.log("server error", err);
 });

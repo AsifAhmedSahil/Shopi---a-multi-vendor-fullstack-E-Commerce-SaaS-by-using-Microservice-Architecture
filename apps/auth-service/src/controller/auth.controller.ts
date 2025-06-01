b01c6df8c79afaae3a30b36493d265dfa035abc6
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-06-01 22:31:30 +0600

    auth helper implemented

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { validateRegistrationData } from "../utils/auth.helper";
+import prisma from "../../../../packages/lib/prisma";
+import { ValidationError } from "../../../../packages/error-handler";
+
+
+export const userRegistration = async (req:Request, res:Response,next:NextFunction) =>{
+
+    validateRegistrationData(req.body,"user")
+
+    const {name,email} = req.body
+
+    const existingUser = await prisma.users.findUnique({ where: email})
+
+    if(existingUser){
+        return next(new ValidationError("User already exist with this email!"))
+    }
+
+  
+
+}

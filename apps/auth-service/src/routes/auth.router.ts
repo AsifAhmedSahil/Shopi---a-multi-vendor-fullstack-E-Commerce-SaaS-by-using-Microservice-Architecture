3f3626255a551f8ad7aefdc565859e73670ec98c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-06-05 22:47:02 +0600

    update:

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -3,7 +3,7 @@ import { userRegistration } from "../controller/auth.controller";
 
 
 const router:Router = express.Router()
-// api written here
+
 
 router.post("/user-registration",userRegistration)
 

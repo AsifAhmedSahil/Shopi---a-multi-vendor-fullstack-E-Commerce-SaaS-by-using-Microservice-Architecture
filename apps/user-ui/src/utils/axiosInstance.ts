997848a023f4177bea942500bc439e58d72f3d8c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-06-12 14:37:40 +0600

    axios instance :

---
+++ apps/user-ui/src/utils/axiosInstance.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URI,
+  withCredentials: true,
+});
+
+let isRefreshing = false;
+let refreshSubscribers: (() => void)[] = [];
+
+// handle logout and prevent infinite loops
+
+const handleLogout = () => {
+  if (window.location.pathname !== "/login") {
+    window.location.href = "/login";
+  }
+};
+
+// handle adding a new refresh token
+
+const subscribeRefreshToken = (callback: () => void) => {
+  refreshSubscribers.push(callback);
+};
+
+// execute queued requests after refresh
+
+const onRefreshSuccess = () => {
+  refreshSubscribers.forEach((callback) => callback());
+  refreshSubscribers = [];
+};
+
+// handle api request
+
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+// handle expire token and refresh logic
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // prevent infinite retry loop
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          subscribeRefreshToken(() => resolve(axiosInstance(originalRequest)));
+        });
+      }
+      originalRequest._retry = true;
+      isRefreshing = true;
+      try {
+        await axios.post(
+          `${process.env.NEXT_PUBLIC_SERVER_URI}/api/refresh-token-user`,
+          {},
+          { withCredentials: true }
+        );
+        isRefreshing = false;
+        onRefreshSuccess();
+
+        return axiosInstance(originalRequest);
+      } catch (error) {
+        isRefreshing = false;
+        refreshSubscribers = [];
+        handleLogout();
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);

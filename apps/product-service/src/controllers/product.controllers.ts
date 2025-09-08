32eb48b9d3c316ea244e5a6b0c24e96397f2a073
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-09-08 23:41:00 +0600

    createDiscountCodes backend function added

--- apps/product-service/src/controllers/product.controllers.ts
+++ apps/product-service/src/controllers/product.controllers.ts
@@ -1,23 +1,63 @@
+import { ValidationError } from "@packages/error-handler";
 import prisma from "@packages/lib/prisma";
 import { NextFunction, Request, Response } from "express";
 
-
-export const getCategories = async(
-    req:Request,res:Response,next:NextFunction
-) =>{
-
-    try {
-        const config = await prisma.site_config.findFirst()
-        if(!config){
-            return res.status(404).json({message:"Categories not found"});
-        }
-
-        return res.status(200).json({
-            categories: config.categories,
-            subCategories: config.subCategories
-        })
-    } catch (error) {
-        console.log(error )
-        
+export const getCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const config = await prisma.site_config.findFirst();
+    if (!config) {
+      return res.status(404).json({ message: "Categories not found" });
     }
-}
+
+    return res.status(200).json({
+      categories: config.categories,
+      subCategories: config.subCategories,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createDiscountCodes = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { public_name, discountType, discountValue, discountCode } = req.body;
+
+    const isDiscountCodeExist = await prisma.discount_codes.findUnique({
+      where: {
+        discountCode,
+      },
+    });
+
+    if (isDiscountCodeExist) {
+      return next(
+        new ValidationError(
+          "Discount code always avaiable please use a different code!"
+        )
+      );
+    }
+    const discount_code = await prisma.discount_codes.create({
+        data:{
+            public_name,
+            discountCode,
+            discountType,
+            discountValue: parseFloat(discountValue),
+            sellerId: req.seller.id 
+        }
+    });
+
+    res.status(201).json({
+        success: true,
+        discount_code,
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
